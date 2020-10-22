2ecf7448f3d68eb32701d8f078641f135608450e
Federico Auster <federicoauster@gmail.com>
2020-10-22 00:59:54 -0300

    Updated settings

--- portfolio_page/settings.py
+++ portfolio_page/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["SECRET_KEY_PORTFOLIO"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG') == 'True')
 
 ALLOWED_HOSTS = [
     "austerportfolio.herokuapp.com",
